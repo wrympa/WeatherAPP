53db12b1ccde6db1be2c69cae75255e02a961250
data shanidze <dshan21@freeuni.edu.ge>
2025-02-08 03:48:40 -0800

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by sento kiryu on 2/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
